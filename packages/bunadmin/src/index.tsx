5429d02abc50641b69724ffb1f5d302d74ee4af9
Chris <eskim0@qq.com>
2021-04-01 22:26:57 +0800

    Update package (add jest-dom), improve test (index page)

--- packages/bunadmin/src/index.tsx
+++ packages/bunadmin/src/index.tsx
@@ -4,9 +4,4 @@ import App from "./App"
 
 export * from "./main"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"))

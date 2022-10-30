181b5358ec03e270bb7632c7206879596f0dbb1b
Chris533 <eskim0@qq.com>
2022-10-30 23:12:46 +0800

    improve(user menu): logout, login props

--- packages/bunadmin/src/components/TopBar/TopBarRightMenu/UserMenu.tsx
+++ packages/bunadmin/src/components/TopBar/TopBarRightMenu/UserMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { ReactElement, useEffect, useState } from "react"
 import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import MenuItem from "@material-ui/core/MenuItem"
@@ -11,16 +11,25 @@ import { useRouter } from "@/router"
 import { Primary } from "@/core/auth/schema"
 import { Trans, useTranslation } from "react-i18next"
 import { BA_DB } from "@/utils/database"
+import { SETTING_NAMES } from "@/utils"
 
 interface State {
   username: string | "Guest"
 }
 
 export type UserMenuProps = {
+  disableSwitch?: boolean // disable `switch account`
+  prepend?: React.ReactNode
+  append?: React.ReactNode
+  LoginOverride?: () => ReactElement
+  onLogin?: () => void
   onProfile?: () => void
   onLogout?: {
     func: () => void
-    override?: boolean
+    funcOverride?: boolean
+    // do action after `logout`; default or undefined is `redirect`
+    actionAfter?: "reload" | "none" | "redirect"
+    redirectTo: string // redirect URL, default is `auth/sign-in`
   }
 }
 
@@ -59,6 +68,14 @@ export default function UserMenu(props: UserMenuProps) {
     router.push(DynamicRoute, route)
   }
 
+  const onLogin = () => {
+    if (props.onLogin) {
+      props.onLogin()
+    } else {
+      router.push(DynamicRoute, UserRoute.signIn)
+    }
+  }
+
   const onProfile = () => {
     props.onProfile?.()
   }
@@ -66,35 +83,58 @@ export default function UserMenu(props: UserMenuProps) {
   const onLogout = async () => {
     if (props.onLogout) {
       props.onLogout.func()
-      if (props.onLogout.override) return
+      if (props.onLogout.funcOverride) return
     }
 
+    // renew user profile
+
+    const db = BA_DB
+    // delete auth
+    await db.users.delete(state.username)
     // update username in setting
-    await BA_DB.settings.put({
+    await db.settings.put({
       name: Primary,
+      value: undefined,
+      updated_at: Date.now()
+    })
+    // update role in setting
+    await db.settings.put({
+      name: SETTING_NAMES.role,
       value: undefined,
       updated_at: Date.now()
     })
 
     handleClose({})
-    router.push(UserRoute.signIn)
+    if (props.onLogout?.actionAfter == "none") return
+
+    if (props.onLogout?.actionAfter == "reload") {
+      window.location.reload()
+    } else {
+      router.push(
+        props.onLogout?.redirectTo
+          ? props.onLogout.redirectTo
+          : UserRoute.signIn
+      )
+    }
   }
 
   return (
     // User Icon
     <div>
       {state.username == "" ? (
-        <Button
-          color="primary"
-          variant="outlined"
-          size="small"
-          onClick={() => {
-            router.push(DynamicRoute, UserRoute.signIn)
-          }}
-          style={{ marginLeft: 12 }}
-        >
-          Sign in
-        </Button>
+        props.LoginOverride ? (
+          <props.LoginOverride />
+        ) : (
+          <Button
+            color="primary"
+            variant="outlined"
+            size="small"
+            onClick={onLogin}
+            style={{ marginLeft: 12 }}
+          >
+            Sign in
+          </Button>
+        )
       ) : (
         <IconButton
           aria-label="account of current user"
@@ -134,15 +174,22 @@ export default function UserMenu(props: UserMenuProps) {
             }}
           />
         </MenuItem>
+        {props.prepend}
         <MenuItem onClick={onProfile}>{t("Profile")}</MenuItem>
         <Divider />
-        <MenuItem onClick={() => handleClose({ route: LocalDataRoute.auth })}>
-          {t("Switch account")}
-        </MenuItem>
-        <MenuItem onClick={() => handleClose({ route: UserRoute.signIn })}>
-          {t("Add another account")}
-        </MenuItem>
-        <Divider />
+        {!props.disableSwitch && (
+          <>
+            <MenuItem
+              onClick={() => handleClose({ route: LocalDataRoute.auth })}
+            >
+              {t("Switch account")}
+            </MenuItem>
+            <MenuItem onClick={onLogin}>{t("Add another account")}</MenuItem>
+            <Divider />
+          </>
+        )}
+
+        {props.append}
         <MenuItem onClick={onLogout}>{t("Logout")}</MenuItem>
       </Menu>
     </div>

29d62ef9a796de9905e085a5240aad856f0612ea
Chris533 <eskim0@qq.com>
2026-05-31 03:40:28 +0000

    chore: set up Tailwind toolchain and remove MUI dependencies
    - Add tailwind.config.js, src/tailwind.css; link compiled output in index.html
    - config-overrides.js: transpile @tanstack ESM (pulled in by @headlessui/react) for CRA4
    - Remove @mui/*, @emotion/*, formik-mui, material-table; add @headlessui/react, @tanstack/react-table; notistack 2->3
    - gitignore generated public/tailwind.out.css

--- packages/bunadmin/config-overrides.js
+++ packages/bunadmin/config-overrides.js
@@ -1,10 +1,29 @@
 const { alias, configPaths } = require("react-app-rewire-alias")
+const path = require("path")
 require("./bunadmin-scripts")
 
 module.exports = function override(config) {
   alias({
     ...configPaths("tsconfig.base.json")
   })(config)
+
+  // CRA4/webpack4 excludes node_modules from babel. Some deps (e.g. the
+  // @tanstack packages pulled in by @headlessui/react) ship modern ESM
+  // (nullish-coalescing) that must be transpiled to parse.
+  config.module.rules.push({
+    test: /\.m?js$/,
+    include: /node_modules[\\/]@tanstack/,
+    use: {
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [require.resolve("babel-preset-react-app")],
+        plugins: [
+          require.resolve("@babel/plugin-proposal-nullish-coalescing-operator"),
+          require.resolve("@babel/plugin-proposal-optional-chaining")
+        ]
+      }
+    }
+  })
 
   return config
 }
